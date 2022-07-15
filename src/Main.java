b79cc94a60d9412fe477708140591f1afdba9639
user <imaginejuli@gmail.com>
2022-07-15 17:31:53 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+//#1 Display only car’s brands & car’s models & car’s price
+//#kg car tablicanyn ichinen tolko mashinanyn brendin jana modelin jana baasyn chygarynyz
+//#2 Display quantity of all cars
+//#kg car tablicasynyn ichinen baardyk mashinalardyn sanyn chygarynyz
+//#3 Display all cars where brand is Hyundai
+//#kg Brendi Hyundaiga barabar bolgon baardyk mashinalardy chygarynyz
+//#4 Display all Red and Blue cars
+//#kg Baardyk Kyzyl jana Kok bolgon mashinalardy chygarynyz
+//#5 Display all cars where issued year between 2000 and 2010
+//#kg Baardyk 2000 menen 2010chu jyldary arasynda chygarylgan mashinalardy chygarynyz
+//#6 Display quantity of cars where car’s brand is Chevrolet
+//#kg Chevrolet brendinde kancha machine bar ekendigin chygarynyz
+//#7 Display the average year of manufacture of cars
+//#kg Mashinalardyn jyldarynyn ortocho maanisin* (srednoe znachenie) tabynyz
+//#8 Display the cars where brand equals to Audi, Toyota, Kia and Ford
+//#kg Audi, Toyota, Kia jana Ford, ushul markadagy baardyk mashinalardy chygargyla
+//#9 Display cars where car’s model starts with ‘T’
+//#kg Mashinanyn modeli ‘T’ menen bashtalgan mashinalardy chygarynyz
+//#10 Display cars where car’s model end with ‘e’;
+//#kg Mashinanyn modeli ‘e’ menen button mashinalardy chygarynyz
+//#11 Display brands where contains only 5 symbols
+//#kg 5 symbol kamtygan brandderdi consolgo chygarynyz
+//#12 Display all amounts where car’s brand is Mercedes-Benz
+//#kg brandi Mercedes-Benz bolgon mashinalardyn obshiy baasyn chygarynyz
+//#13 Display the most expensive car & the sheepest car
+//#kg en kymbet jana en arzan mashinany chygarynyz
+//#14 Display all cars where car’s brand not TOYOTA
+//#kg TOYOTAdan bashkasynyn baaryn chygarynyz
+//#15 Display 10 the most expensive cars
+//#kg En kymbat 10 mahsinany chygarynyz
+//#16 Display the newest cars between 5th to 15th
+//#kg En jany mashinalardyn 5ten bashtap 15chige cheinki mashinalardy chygarynyz
+//#17 Display cars where car’s year of issue not between 1995 and 2005;
+//#kg 1995 jana 2005 jyldar arasyndagy mashinalardan bashka baardyk mashinalardy chygarynyz
+//#18 Display most cars in one color
+//#kg en kop mashinasy bar tustordu korsotunuzdor i kancha mashinesi bar ekendigin dagy korsotunuzdor
+    }
+}
